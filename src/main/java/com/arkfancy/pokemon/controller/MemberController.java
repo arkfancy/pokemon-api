a1fba7a912a7ca8e88e9d3634e9e70010de7cec1
arkfancy <ark_light_0@163.com>
2019-11-26 17:10:32 +0800

    用户离开 用户修改级别

--- src/main/java/com/arkfancy/pokemon/controller/MemberController.java
+++ src/main/java/com/arkfancy/pokemon/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.arkfancy.pokemon.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +18,7 @@ import com.arkfancy.pokemon.entity.Member;
 import com.arkfancy.pokemon.service.MemberService;
 import com.arkfancy.pokemon.support.util.IDCreator;
 import com.baomidou.kisso.annotation.Login;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.api.ApiController;
 import com.baomidou.mybatisplus.extension.api.R;
@@ -48,6 +52,24 @@ public class MemberController extends ApiController {
 //		return success(new Member().selectPage(new Page<>(current, size),
 //				new QueryWrapper<Member>().isNull(!containLeave, Member.LEAVE_DATE).orderByAsc(Member.ID)));
 		return success(memberService.selectMemberPage(new Page<>(current, size), containLeave));
+	}
+
+	@PutMapping("/leave/{memberId}/{leaveDate}")
+	public R<Boolean> leave(@PathVariable Integer memberId, @PathVariable(required = false) String leaveDate) {
+		Member member = new Member();
+		if (leaveDate == null || leaveDate.isEmpty()) {
+			member.setLeaveDate(LocalDate.now());
+		} else {
+			member.setLeaveDate(LocalDate.parse(leaveDate, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+		}
+		return success(memberService.update(member, new UpdateWrapper<Member>().eq(Member.MEMBER_ID, memberId)));
+	}
+
+	@PutMapping("/rank/{memberId}/{rankId}")
+	public R<Boolean> leave(@PathVariable Integer memberId, @PathVariable Integer rankId) {
+		Member member = new Member();
+		member.setRankId(rankId);
+		return success(memberService.update(member, new UpdateWrapper<Member>().eq(Member.MEMBER_ID, memberId)));
 	}
 
 	@PostMapping("")
