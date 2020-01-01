ccb33b62cdf0816e40a7fcff3875b1f98d6f8396
arkfancy <ark_light_0@163.com>
2020-01-01 23:33:32 +0800

    新增公会战记录接口

--- src/main/java/com/arkfancy/pokemon/controller/GuildWarController.java
+++ src/main/java/com/arkfancy/pokemon/controller/GuildWarController.java
@@ -1,10 +1,18 @@
 package com.arkfancy.pokemon.controller;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import com.arkfancy.pokemon.entity.GuildWar;
+import com.arkfancy.pokemon.service.GuildWarService;
+import com.baomidou.kisso.annotation.Login;
 import com.baomidou.mybatisplus.extension.api.ApiController;
+import com.baomidou.mybatisplus.extension.api.R;
 
 /**
  * <p>
@@ -18,5 +26,13 @@ import com.baomidou.mybatisplus.extension.api.ApiController;
 @RequestMapping("/guild-war")
 public class GuildWarController extends ApiController {
 
-}
+	@Autowired
+	private GuildWarService guidWarService;
 
+	@PostMapping("/list")
+	@Login
+	public R<List<GuildWar>> insertList(@RequestBody List<GuildWar> guildWars) {
+		guidWarService.saveBatch(guildWars);
+		return success(guildWars);
+	}
+}
